00144a5a2c81bce17d56978621715cd3055bf1aa
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-12-06 15:37:45 -0800

    added logic to race

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log('JS is working');
+
+//$(document).ready(function() {
+
+
+//});
+
+
+
+function myFunction(event) {
+    var a = event.keyCode;
+    if (a == 65) {  // 65 is the a key
+        console.log("go!");
+    }
+}
+
+function myFunction(event) {
+    var l = event.keyCode;
+    if (l == 76) {  // 76 is the l key
+        console.log("go!");
+    }
+}
+
+// var user1 = 0;
+// var user2 = 0;
+
+// let user1Winner = function () {
+	// if (user1 == 6 && user1 >= user2);
+ // alert("Fish #1 is the winner!");
+// } else {
+	 // break;
+	// };
+
+// let user2Winner = function () {
+	// if (user2 == 6 && user2 >= user1);
+// alert("Fish #1 is the winner!");
+// } else {
+  // break;
+// };
