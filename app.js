509448304ed193dff1f8471644a9f3dd2500ff74
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-12-07 09:04:20 -0800

    attempt logic

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ console.log('JS is working');
 //$(document).ready(function() {
 
 
+
+
 //});
 
 
+// function myFunction(event) {
+    // var a = event.keyCode;
+    // if (a == 65) {  // 65 is the a key
+        // console.log("go!");
+    // }
+// }
 
-function myFunction(event) {
-    var a = event.keyCode;
-    if (a == 65) {  // 65 is the a key
-        console.log("go!");
-    }
-}
-
-function myFunction(event) {
-    var l = event.keyCode;
-    if (l == 76) {  // 76 is the l key
-        console.log("go!");
-    }
-}
+// function myFunction(event) {
+    // var l = event.keyCode;
+    // if (l == 76) {  // 76 is the l key
+        // console.log("go!");
+    // }
+// }
 
 // var user1 = 0;
 // var user2 = 0;
